d792282e60e7bdd578ae6d636aa308a4bbab95d6
Ailuridaes <ailuridaes@gmail.com>
2017-12-08 13:19:42 -0800

    Update KCS State model to include labels

--- models/kcsState.model.js
+++ models/kcsState.model.js
@@ -1,5 +1,23 @@
 export const kcsStateModel = [
-    { field: 'visibility' },
-    { field: 'confidence' },
-    { field: 'flagged', transform: 'boolean' }
+    {
+        field: 'visibility', transform: [
+            { field: '@label', name: 'label' },
+            { field: 'value' },
+            { field: 'value-label' }
+        ] 
+    },
+    {
+        field: 'confidence', transform: [
+            { field: '@label', name: 'label' },
+            { field: 'value' },
+            { field: 'value-label' }
+        ] 
+    },
+    {
+        field: 'flagged', transform: [
+            { field: '@label', name: 'label' },
+            { field: 'value', transform: 'boolean' },
+            { field: 'value-label' }
+        ] 
+    }
 ];
